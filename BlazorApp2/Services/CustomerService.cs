c5b162c7ce095ed671c24002fb1d8e9226c4f7ca
Kinzy Moussi <S2302172@ndonline.ac.uk>
2024-11-04 12:34:05 +0000

    Added a login page and UI for it

--- BlazorApp2/Services/CustomerService.cs
+++ BlazorApp2/Services/CustomerService.cs
@@ -1,6 +1,7 @@
 ﻿using BlazorApp2.Models;
 using MudBlazor;
 using BlazorApp2.Services;
+using Microsoft.EntityFrameworkCore;
 
 
 namespace BlazorApp2.Services
@@ -18,6 +19,11 @@ namespace BlazorApp2.Services
         {
             await _context.Customers.AddAsync(customer);
             await _context.SaveChangesAsync();
+        }
+        public async Task<Customer?> LogIn(Customer customer)
+        {
+            return await _context.Customers.FirstOrDefaultAsync(
+                c => c.Username == customer.Username && c.Password == customer.Password);
         }
     }
 
