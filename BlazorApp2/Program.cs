b47ecaa9494a67bfdbd970e6d0d3837966633ef7
Kinzy Moussi <S2302172@ndonline.ac.uk>
2024-11-18 13:15:51 +0000

    Corrected tickets.razor C# code

--- BlazorApp2/Program.cs
+++ BlazorApp2/Program.cs
@@ -23,8 +23,8 @@ namespace BlazorApp2
             builder.Services.AddSingleton<UserSession>();
 
             builder.Services.AddScoped<CustomerService>();
-            builder.Services.AddScoped<RoomService>();
-            builder.Services.AddScoped<RoombookingService>();
+            //builder.Services.AddScoped<RoomService>();
+            //builder.Services.AddScoped<RoombookingService>();
             builder.Services.AddScoped<AttractionService>();
             builder.Services.AddSingleton<UserSession>();
 
