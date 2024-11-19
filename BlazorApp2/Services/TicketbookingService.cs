c3ee1f79cb534ea51eceb42f6c6d462c5042435f
Kinzy Moussi <S2302172@ndonline.ac.uk>
2024-11-19 09:33:59 +0000

    Added TicketbookingService w/ C# code

---
+++ BlazorApp2/Services/TicketbookingService.cs
@@ -0,0 +1,23 @@
+﻿using BlazorApp2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorApp2.Services
+{
+    public class TicketbookingService
+    {
+        private readonly TlS2302172RzaContext _context;
+        public TicketService(TlS2302172RzaContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Ticket>> GetTicketsAsync()
+        {
+            return await _context.Tickets.ToListAsync();
+        }
+        public async Task AddTicketAsync(Ticket newTicket)
+        {
+            await _context.Tickets.AddAsync(newTicket);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
